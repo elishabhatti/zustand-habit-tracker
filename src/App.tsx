531352ae6422748bc096c57c35721ea57ca374f0
elisha <elishajameel2007@gmail.com>
2025-07-26 14:49:24 +0500

    removed fetched habits

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-import { useEffect } from "react";
 import AddHabitForm from "./components/add-habit-form";
 import HabitList from "./components/habit-list";
-import useHabitStore from "./store/store";
 import { Box, Container, Typography } from "@mui/material";
 
 const App = () => {
-  const {fetchHabits} = useHabitStore();
   
-  useEffect(() => {
-    fetchHabits()
-  },[])
-
   return (
     <Container>
       <Box>
